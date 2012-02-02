da6085b06db6075cbaa952a1c889edb4b82456e6
Benoit Bayol <benoit.bayol@gmail.com>
2012-02-02 10:39:59 +0700

    added tests for factorial, point, square

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,123 @@
+/*
+  The purpose of this document is to learn basic C++ syntax and semantics through some already written test.
+ */
+
+// iostream allows users to interact with terminal
 #include <iostream>
-int fact( int n)
+
+/*--------------------------------------------------------------------------------------------------------------*/
+/* FACTORIAL */
+//write function factorial below this line
+int factorial(int n)
 {
     if (n==0)
-    return 1;
-    else return n*fact(n-1);
+        return 1;
+    else return n*factorial(n-1);
 }
 
+bool factorial_test()
+{
+    return (6 == factorial(3));
+}
+
+/*--------------------------------------------------------------------------------------------------------------*/
+/* POINT */
+
+//write class point below this line
+
+/*
+//Test if x() and y() method returs values of x an y private attributes
+bool point_test_1()
+{
+    int x = 10;
+    int y = 12;
+    Point p(x,y);
+    return (p.x() == x &&  p.y() == y);
+}
+*/
+
+ /*
+//Test if move() method works correctly
+//We move position to the position given to move()
+bool point_test_2()
+{
+    int x = 0;
+    int y = 0;
+    Point p(x,y);
+    p.move(1,1);
+    return (p.x() == 1 && p.y() == 1);
+}
+ */
+
+  /*
+void point_test_3()
+{
+    Point p0(12, 15);
+    Point p1(13, 14);
+    p0.print();
+    p1.print();
+    std::cout << "p0 should print : (12,15)" << std::endl;
+    std::cout << "p1 should print : (13,14)" << std::endl;
+}
+  */
+
+   /*
+bool point_test()
+{
+    //std::cout << "point_test_1 " << point_test_1() << std::endl;
+    //std::cout << "point_test_2 " << point_test_2() << std::endl;
+    //std::cout << "point_test_3 " << point_test_3() << std::endl;
+}
+   */
+/*--------------------------------------------------------------------------------------------------------------*/
+/* SQUARE */
+
+/*
+bool square_test_1()
+{
+    Point center(10,10);
+    int length = 2;
+    Square s(center, length);
+    return (s.center().x() == 10 && s.center().y() == 10 && s.length() == 2);
+}
+*/
+
+ /*
+bool square_test_2()
+{
+    Point center(10,10);
+    int length = 2;
+    Square s(center, length);
+    s.move(15,15)
+    return (s.center().x() == 15 && s.center().y() == 15);
+}
+ */
+
+  /*
+bool square_test_3()
+{
+    Point center(10,10);
+    int length = 2;
+    Square s(center, length);
+    s.print();
+    std::cout << "s should print : (10,10) - 2" << std::endl;
+}
+  */
+
+   /*
+bool square_test()
+{
+    //std::cout << "square_test_1 " << square_test_1() << std::endl;
+    //std::cout << "square_test_2 " << square_test_2() << std::end;
+    //std::cout << "square_test_3 " << square_test_3() << std::end;
+}
+   */
+
+/*--------------------------------------------------------------------------------------------------------------*/
+/*ALL TESTS*/
 int main(int argc, char ** argv)
 {
-
-    int a;
-    a=fact(4);
-    std::cout << a << std::endl;
-} 
+    std::cout << "factorial_test " << std::endl << factorial_test() << std::endl;
+    //std::cout << "point_test " << std::endl << point_test() << std::endl;
+    //std::cout << "square_test" << std::endl << square_test() << std::endl;
+}
